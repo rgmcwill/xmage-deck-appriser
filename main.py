213341e0498de000811120d6ee36de1386a17b4f
rgmcwill <rossmcwilliams360@gmail.com>
2021-04-28 14:52:45 -0500

    html look presentable

--- main.py
+++ main.py
@@ -13,24 +13,8 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('deck', deck_name=filename[:-4]))
-    print(UPLOAD_FOLDER)
+@app.route('/')
+def index():
     return render_template('index.html')
 
 def proccess_line(line):
@@ -68,35 +52,11 @@ def deck(deck_name):
     deck_name=deck_name+'.dck'
     a = proccess_dck(deck_name)
     cost = 0
-    # for k,v in a['MB'].items():
-    #     response = requests.get('https://api.scryfall.com/cards/search?q=' + k)
-    #     dic = response.json()
-    #     print(response.status_code)
-    #     b = dic['data'][0]['prices']['usd']
-    #     if b != None:
-    #         card_cost  = float(b)
-    #         cost = cost + card_cost
-    #     time.sleep(0.75)
-    # for k,v in a['SB'].items():
-    #     response = requests.get('https://api.scryfall.com/cards/search?q=' + k)
-    #     dic = response.json()
-    #     print(response.status_code)
-    #     b = dic['data'][0]['prices']['usd']
-    #     if b != None:
-    #         card_cost  = float(b)
-    #         cost = cost + card_cost
-    #     time.sleep(0.75)
     to_return = str(a) + '<div></div>' + str(cost)
     temp = {}
     temp.update(a.get('MB'))
     temp.update(a.get('SB'))
     return render_template('deck.html', result=temp)
-
-@app.route('/_add_numbers')
-def add_numbers():
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
 
 @app.route('/price_card')
 def price_card():
@@ -123,6 +83,22 @@ def price_card():
     print(card_name, card_id, card_price)
     return jsonify(card_name=card_name, card_id=card_id, card_price=card_price, card_image_url=card_image_url)
 
-@app.route('/test')
-def index():
-    return render_template('deck.html')
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('deck', deck_name=filename[:-4]))
+    print(UPLOAD_FOLDER)
+    return render_template('upload.html')
